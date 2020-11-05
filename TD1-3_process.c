51f2d932cc13fe94f172f2ab697006d67c44a95d
mxcezl <54847071+mxcezl@users.noreply.github.com>
2020-11-05 08:47:57 +0000

    Create TD1-3_process.c

---
+++ TD1-3_process.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void comparer_pere_fils() {
+    char repertoire[255];
+    pid_t pid = fork();
+    if(pid == -1) perror("Erreur a l'appel de fork");
+    else if (pid == 0) {
+        printf("[*] Processus fils\n");
+        printf("S'endort pendant 2 secondes...\n\n", );
+        sleep(2);
+    } else printf("[*] Processus pere\n\n");
+    printf("PID : %i\nPID père : %i\nRepertoire de travail : %s\nPropriétaire réel : %i\nPropriétaire effectif : %i\nGroupe propriétaire réel : %i\nGroupe propriétaire effectif : %i\n\n", getpid(), getppid(), getcwd(repertoire, 255), getuid(), geteuid(), getgid(), getegid());
+}
+
+int main()
+{
+    comparer_pere_fils();
+    return 0;
+}
